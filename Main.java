9c82899c544029292ecb777d657b289535af1e61
Latifa M <94848902+l-atifa@users.noreply.github.com>
2022-06-13 13:09:51 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+
+  /*String words[] = {"ten", "fading", "post", "card", "thunder", "hinge", "trailing", "batting"}; 
+
+  for(String s : words)
+  {
+    if(s.substring(s.length()-3) == "ing")
+    {
+      System.out.println(s);
+    }
+  }*/
+
+  
+    
+    
+
+}
